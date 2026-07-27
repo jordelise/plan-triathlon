d1cdf3f74f34247fb487d2f7a19710635d971c12
Jordelise <elisejord@gmail.com>
2026-07-27 15:52:39 +0200

    Use woman variant emoji for swim/bike/run icons
    Swapped generic swimmer/cyclist/runner emoji for the woman-presenting
    variants throughout the app (home splits, sport filter toggle, and
    all session icons in the DB).

---
+++ supabase/use_woman_emoji_icons.sql
@@ -0,0 +1,3 @@
+update plan_session_completions set icon = '🏊‍♀️' where icon = '🏊';
+update plan_session_completions set icon = '🚴‍♀️' where icon = '🚴';
+update plan_session_completions set icon = '🏃‍♀️' where icon = '🏃';
